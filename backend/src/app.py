b5e6b5f0c2dbf837029270a8e15d618867271f84
Winggo Tse <winggo.tse@gmail.com>
2025-04-04 22:01:08 -0700

    seperate socket routess

--- backend/src/app.py
+++ backend/src/app.py
@@ -14,44 +14,14 @@ socketio = SocketIO(app, cors_allowed_origins='*')
 
 r_client, pubsub = start_redis_client(socketio)
 app.config['REDIS'] = r_client
+app.config['PUBSUB'] = pubsub
+
 
 from api import api_routes
 app.register_blueprint(api_routes, url_prefix="/api")
 
-
-client_subscriptions = {}
-
-@socketio.on("connect")
-def handle_connect():
-    print(f"Client {request.sid} connected")
-
-
-@socketio.on('disconnect')
-def handle_disconnect():
-    print(f"Client {request.sid} disconnected")
-
-    channels = client_subscriptions.pop(request.sid, [])
-    for channel in channels:
-        pubsub.unsubscribe(channel)
-
-
-@socketio.on("subscribe")
-def handle_redis_subscribe(channel):
-    print(f"Client {request.sid} subscribed to {channel}")
-
-    if request.sid not in client_subscriptions:
-        client_subscriptions[request.sid] = []
-    client_subscriptions[request.sid].append(channel)
-    pubsub.subscribe(channel)
-
-
-@socketio.on("unsubscribe")
-def handle_redis_unsubscribe(channel):
-    print(f"Client {request.sid} unsubscribed from {channel}")
-    
-    if request.sid in client_subscriptions and channel in client_subscriptions[request.sid]:
-        client_subscriptions[request.sid].remove(channel)
-        pubsub.unsubscribe(channel)
+from sockets import socket_routes
+app.register_blueprint(socket_routes)
 
 
 if __name__ == "__main__":
