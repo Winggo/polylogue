8277b0cf130607f070ac38b468da25e3b2dade39
Winggo Tse <winggo.tse@gmail.com>
2025-04-09 03:32:04 -0700

    add write and update routes to db

--- backend/src/db/firestore.py
+++ backend/src/db/firestore.py
@@ -2,15 +2,38 @@ from google.cloud import firestore
 
 
 def start_firestore_project_client(project):
-    ds = firestore.Client(project=project)
-    return ds
+    db = firestore.Client(project=project)
+    return db
 
 
-def get_document_by_collection_and_id(ds, collection_name, doc_id):
-    collection = ds.collection(collection_name)
+def get_document_by_collection_and_id(db, collection_name, doc_id):
+    collection = db.collection(collection_name)
     doc_ref = collection.document(doc_id)
     doc = doc_ref.get()
     if doc.exists:
         return doc.to_dict()
     else:
+        raise ValueError(f"Document {doc_id} does not exist in {collection_name}")
+
+
+def save_document_in_collection(db, collection_name, document, doc_id=None):
+    collection = db.collection(collection_name)
+    
+    if doc_id:
+        doc_ref = collection.document(doc_id)
+        doc_ref.set(document)
+        return doc_id
+    else:
+        doc_ref = collection.add(document)[0]
+        return doc_ref.id
+
+
+def update_document_in_collection(db, collection_name, document, doc_id):
+    collection = db.collection(collection_name)
+    
+    try:
+        doc_ref = collection.document(doc_id)
+        doc_ref.update(document)
+        return doc_id
+    except:
         raise ValueError(f"Document {doc_id} does not exist in {collection_name}")
--- backend/src/routes/datastore.py
+++ backend/src/routes/datastore.py
@@ -1,19 +1,69 @@
 from flask import Blueprint, jsonify, request, current_app
-from db.firestore import get_document_by_collection_and_id
+from db.firestore import (
+    get_document_by_collection_and_id,
+    save_document_in_collection,
+    update_document_in_collection,
+)
 
 
 ds_routes = Blueprint("ds_routes", __name__)
 
 
-@ds_routes.route("/v1/canvases/<canvas_id>", methods=["GET"])
-def get_canvas(canvas_id):
-    """Get a canvas document from datastore"""
-    fs = current_app.config['FIRESTORE']
+@ds_routes.route("/v1/canvases", methods=["POST"])
+def canvases_operations():
+    """Routes to read/write to canvases collection"""
+    db = current_app.config['FIRESTORE']
 
-    try:
-        canvas_doc = get_document_by_collection_and_id(fs, "canvases", canvas_id)
-    except Exception as e:
-        print(e)
+    def save_canvas():
+        """Save a canvas document to datastore"""
+        data = request.json
+        try:
+            doc_id = save_document_in_collection(db, "canvases", data)
+        except Exception as e:
+            return jsonify({"error": "Internal Server Error"}), 500
+        
+        return jsonify({"id": doc_id}), 200
+
+
+    if request.method == "POST":
+        return save_canvas()
+    else:
         return jsonify({"error": "Internal Server Error"}), 500
+
+
+@ds_routes.route("/v1/canvases/<canvas_id>", methods=["GET", "PUT"])
+def canvas_operations(canvas_id):
+    """Routes to read/write to single canvas in collection"""
+    db = current_app.config['FIRESTORE']
+
+    def get_canvas(id):
+        """Get a canvas document from datastore"""
+        try:
+            canvas_doc = get_document_by_collection_and_id(db, "canvases", id)
+        except ValueError as e:
+            return jsonify({"error": str(e)}), 400
+        except Exception as e:
+            return jsonify({"error": "Internal Server Error"}), 500
+        
+        return jsonify({"document": canvas_doc}), 200
     
-    return jsonify({"document": canvas_doc}), 200
+    
+    def update_canvas(id):
+        """Update a canvas document in datastore"""
+        data = request.json
+        try:
+            doc_id = update_document_in_collection(db, "canvases", data, doc_id=id)
+        except ValueError as e:
+            return jsonify({"error": str(e)}), 400
+        except Exception as e:
+            return jsonify({"error": "Internal Server Error"}), 500
+        
+        return jsonify({"id": doc_id}), 200
+    
+
+    if request.method == "GET":
+        return get_canvas(canvas_id)
+    elif request.method == "PUT":
+        return update_canvas(canvas_id)
+    else:
+        return jsonify({"error": "Internal Server Error"}), 500
