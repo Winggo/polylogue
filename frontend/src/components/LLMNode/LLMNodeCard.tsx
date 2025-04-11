1eb4f9fcab2f3f1f50e2d20899e1b97ce8f19cf9
Winggo Tse <winggo.tse@gmail.com>
2025-04-11 07:17:22 -0700

    reformat empty card instructions

--- frontend/src/components/LLMNode/LLMNodeCard.tsx
+++ frontend/src/components/LLMNode/LLMNodeCard.tsx
@@ -1,7 +1,7 @@
 export default function LLMNodeCard() {
     return (
         <div className="w-full h-full flex flex-col items-center justify-around llm-node-card">
-            <div className="cursor-text select-text">
+            <div className="cursor-text select-text mt-[50px]">
                 <span>You can...</span>
                 <br />
                 <span>- create a packing list 🏕️</span>
@@ -12,12 +12,12 @@ export default function LLMNodeCard() {
                 <br />
                 <span>- write a love letter 🌹</span>
             </div>
-            <div className="cursor-text select-text text-center">
-                <span>Click on → icon and drag to connect nodes</span>
-                <br />
-                <span>Click on ⋮⋮⋮ and drag to move node</span>
+            <div className="cursor-text select-text text-center mb-[100px]">
+                <span>Click on → icon to create a connected node</span>
                 <br />
                 <span>Press ⇥ to use suggested prompt</span>
+                <br />
+                <span>Click on ⋮⋮⋮ and drag to move node</span>
                 <br />
                 <span>Press ⌫ to delete selected node</span>
             </div>
